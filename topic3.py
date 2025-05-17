c52293fc94d9446d2b5f7a8adca59335b31e1939
vennela <vennelatata06@gmail.com>
2025-05-17 13:27:35 +0530

    Add Topic 3 control flow & loops examples

---
+++ topic3.py
@@ -0,0 +1,21 @@
+# Topic 3 exercises 
+# Topic 3: Control Flow & Loops
+
+# Task 1: if/elif/else
+x = 10
+if x < 5:
+    print("Less than 5")
+elif x == 10:
+    print("Exactly 10")
+else:
+    print("Greater than 5 and not 10")
+
+# Task 2: for loop (print 1 through 5)
+for i in range(1, 6):
+    print(i)
+
+# Task 3: while loop (count 0 to 2)
+count = 0
+while count < 3:
+    print("Count is", count)
+    count += 1
